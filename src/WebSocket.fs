139b7662e63fbdd2d0030e9b811838cae8357989
Dag Brattli <dag@brattli.net>
2018-09-16 09:59:25 +0200

    Some fixes and debug added

--- src/WebSocket.fs
+++ src/WebSocket.fs
@@ -1,4 +1,4 @@
-namespace Fable.Reaction.WebSocket
+namespace Fable.Reaction
 
 open Fable.Core.JsInterop
 open Fable.Import.Browser
@@ -7,7 +7,7 @@ open Reaction
 
 module WebSocket =
 
-    exception Error of string
+    exception WSError of string
 
     /// Websocket operator.
     let websocket (uri: string) (source: AsyncObservable<'msg>) : AsyncObservable<'msg> =
@@ -18,19 +18,26 @@ module WebSocket =
 
                 let _obv n = async {
                     match n with
-                    | OnNext x -> websocket.send x
+                    | OnNext msg ->
+                        printfn "Sending %A" msg
+                        websocket.send msg
                     | OnError ex ->
+                        printfn "OnError: closing"
                         websocket.close ()
                     | OnCompleted ->
+                        printfn "OnCompleted: closing"
                         websocket.close ()
                 }
 
                 let onMessage (ev : MessageEvent) =
                     let msg = ofJson<'msg'> (string ev.data)
-                    Async.RunSynchronously (OnNext msg |> obv)
+                    Async.StartImmediate (OnNext msg |> obv)
 
                 let onOpen _ =
+                    printfn "onOpen"
+
                     let action = async {
+                        printfn "Subscribing upstream"
                         let! disposable' = source.SubscribeAsync _obv
                         disposable <- AsyncDisposable.Unwrap disposable'
                     }
@@ -38,11 +45,14 @@ module WebSocket =
                     Async.StartImmediate action
 
                 let onError ev =
-                    let ex = Error (ev.ToString ())
-                    Async.RunSynchronously (OnError ex |> obv)
+                    printfn "onError: %A" ev
+
+                    let ex = WSError (ev.ToString ())
+                    Async.StartImmediate (OnError ex |> obv)
 
                 let onClose ev =
-                    Async.RunSynchronously (obv OnCompleted)
+                    printfn "onClose: %A" ev
+                    Async.StartImmediate (obv OnCompleted)
 
                 websocket.onmessage <- onMessage
                 websocket.onclose <- onClose
