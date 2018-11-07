6dcfb69f6f43ae3a40f5d9b498078a546c981c49
Dag Brattli <dag@brattli.net>
2018-11-07 20:35:01 +0100

    Client rewritten to Fable.Elmish.Reaction prerelease

--- src/Client/Magic.fs
+++ src/Client/Magic.fs
@@ -15,9 +15,9 @@ open Shared
 open Fulma
 open Fulma.Extensions
 
-open Fable.Reaction
-open Fable.Reaction.WebSocket
-open Reaction
+open Reaction.AsyncRx
+open Elmish.Reaction
+open Elmish.Reaction.WebSocket
 open Shared
 
 type LetterSource =
@@ -51,18 +51,20 @@ let init =
 
 let loadCountCmd () =
   ofPromise (fetchAs<int> "/api/init" Decode.int [])
-  |> AsyncObservable.map (Ok >> InitialCountLoaded)
-  |> AsyncObservable.catch (Error >> InitialCountLoaded >> AsyncObservable.single)
+  |> AsyncRx.map (Ok >> InitialCountLoaded)
+  |> AsyncRx.catch (Error >> InitialCountLoaded >> AsyncRx.single)
 
 let withToggledLetters model =
   match model.Letters with
   | None ->
+      printfn "None"
       { model with Letters = Local Map.empty }
 
   | Remote letters ->
       { model with Letters = Local letters }
 
   | Local _ ->
+      printfn "Letters=None"
       { model with Letters = None }
 
 let withToggledRemoterLetters model =
@@ -78,17 +80,21 @@ let withToggledRemoterLetters model =
 
 
 let update (msg : Msg) (model : Model) : Model =
+  //printfn "Magic.update: %A" msg
   match model, msg with
   | App appModel, LetterStringChanged str ->
       { appModel with LetterString = str}
       |> App
 
   | App appModel, ToggleLetters ->
+      printfn "model.Letters %A" appModel.Letters
+
       appModel
       |> withToggledLetters
       |> App
 
   | App appModel, ToggleRemoteLetters ->
+      printfn "model.Letters %A" appModel.Letters
       appModel
       |> withToggledRemoterLetters
       |> App
@@ -259,45 +265,48 @@ let letterStream letterString =
   letterString
   |> Seq.toList // Split into list of characters
   |> Seq.mapi (fun i c -> i, c) // Create a tuple with the index
-  |> AsyncObservable.ofSeq // Make this an observable
-  |> AsyncObservable.flatMap (fun (i, letter) ->
+  |> AsyncRx.ofSeq // Make this an observable
+  |> AsyncRx.flatMap (fun (i, letter) ->
       ofMouseMove ()
-      |> AsyncObservable.delay (100 * i)
-      |> AsyncObservable.map (fun ev -> (i, { Letter = string letter; X = ev.clientX; Y = ev.clientY }))
+      |> AsyncRx.delay (100 * i)
+      |> AsyncRx.map (fun ev -> (i, { Letter = string letter; X = ev.clientX; Y = ev.clientY }))
   )
+
 
 let query (model : Model) msgs =
   match model with
   | App appModel ->
       match appModel.Letters with
       | Local _ ->
-          appModel.LetterString
-          |> letterStream
-          |> AsyncObservable.map Letter
-          , (appModel.LetterString + "_local")
+          let xs =
+            appModel.LetterString
+            |> letterStream
+            |> AsyncRx.map Letter
+          Subscribe (xs, appModel.LetterString + "_local")
 
        | Remote _ ->
           let letterQuery =
             appModel.LetterString
               |> letterStream
-              |> AsyncObservable.map Shared.Msg.Letter
+              |> AsyncRx.map Shared.Msg.Letter
 
           let letterStringQuery =
             msgs
-            |> AsyncObservable.choose (function | LetterStringChanged str -> Some str | _ -> Option.None)
-            |> AsyncObservable.map Shared.Msg.LetterString
+            |> AsyncRx.choose (function | LetterStringChanged str -> Some str | _ -> Option.None)
+            |> AsyncRx.map Shared.Msg.LetterString
 
-          letterStringQuery
-          |> AsyncObservable.merge letterQuery
-          |> server
-          |> AsyncObservable.map RemoteMsg
-          , (appModel.LetterString + "_remote")
+          let xs =
+            letterStringQuery
+            |> AsyncRx.merge letterQuery
+            |> server
+            |> AsyncRx.map RemoteMsg
+          Subscribe (xs, appModel.LetterString + "_remote")
 
       | _ ->
-            AsyncObservable.empty (),"none"
+            Dispose
 
   | Loading ->
-      loadCountCmd (),"loading"
+      Subscribe (loadCountCmd (), "loading")
 
 
 
