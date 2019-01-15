cf2d09556707295396b919635f6d3b3a04139705
Dag Brattli <dag@brattli.net>
2019-01-15 11:47:41 +0100

    Refactored. Remove inner function. Use curry instead.

--- AsyncRx/src/Tap.fs
+++ AsyncRx/src/Tap.fs
@@ -5,30 +5,29 @@ open Core
 [<RequireQualifiedAccess>]
 module Tap =
     /// Tap asynchronously into the stream performing side effects by the given async actions.
-    let tapAsync (onNextAsync: 'a -> Async<unit>) (onErrorAsync: exn -> Async<unit>) (onCompletedAsync: unit -> Async<unit>) : IAsyncObservable<'a> -> IAsyncObservable<'a> =
-        let tapAsyncInner (source: IAsyncObservable<'a>) : IAsyncObservable<'a> =
-            let subscribeAsync (obvAsync : IAsyncObserver<'a>) =
-                async {
-                    let _obv =
-                        { new IAsyncObserver<'a> with
-                            member this.OnNextAsync x = async {
-                                // Let exceptions bubble to the top
-                                do! onNextAsync x
-                                do! obvAsync.OnNextAsync x
-                            }
-                            member this.OnErrorAsync err = async {
-                                do! onErrorAsync err
-                                do! obvAsync.OnErrorAsync err
-                            }
-                            member this.OnCompletedAsync () = async {
-                                do! onCompletedAsync ()
-                                do! obvAsync.OnCompletedAsync ()
-                            }
+    let tapAsync (onNextAsync: 'a -> Async<unit>) (onErrorAsync: exn -> Async<unit>) (onCompletedAsync: unit -> Async<unit>)
+        (source: IAsyncObservable<'a>) : IAsyncObservable<'a> =
+        let subscribeAsync (obvAsync : IAsyncObserver<'a>) =
+            async {
+                let _obv =
+                    { new IAsyncObserver<'a> with
+                        member this.OnNextAsync x = async {
+                            // Let exceptions bubble to the top
+                            do! onNextAsync x
+                            do! obvAsync.OnNextAsync x
                         }
-                    return! source.SubscribeAsync _obv
-                }
-            { new IAsyncObservable<'a> with member __.SubscribeAsync o = subscribeAsync o }
-        tapAsyncInner
+                        member this.OnErrorAsync err = async {
+                            do! onErrorAsync err
+                            do! obvAsync.OnErrorAsync err
+                        }
+                        member this.OnCompletedAsync () = async {
+                            do! onCompletedAsync ()
+                            do! obvAsync.OnCompletedAsync ()
+                        }
+                    }
+                return! source.SubscribeAsync _obv
+            }
+        { new IAsyncObservable<'a> with member __.SubscribeAsync o = subscribeAsync o }
 
     /// Tap asynchronously into the stream performing side effects by the given `onNextAsync` action.
     let tapOnNextAsync (onNextAsync: 'a -> Async<unit>) : IAsyncObservable<'a> -> IAsyncObservable<'a> =
