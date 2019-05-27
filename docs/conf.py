7ca2d05297ee2f143f19f931b324a0c46ede9efe
Dag Brattli <dag.brattli@cognite.com>
2019-05-27 23:34:16 +0200

    Enable dooble for mable diagrams

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ release = '2.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark', 'sphinxcontrib.fsharp', 'alabaster']
+extensions = ['recommonmark', 'sphinxcontrib.fsharp', 'sphinxcontrib_dooble', 'alabaster']
 
 source_suffix = '.rst'
 
