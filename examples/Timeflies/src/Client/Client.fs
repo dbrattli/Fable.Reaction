2e02d371ee83f4496b9b577d07796635ce74e5d8
Dag Brattli <dag.brattli@cognite.com>
2020-05-17 07:07:53 +0200

    Update Timeflies example

--- examples/Timeflies/src/Client/Client.fs
+++ examples/Timeflies/src/Client/Client.fs
@@ -95,5 +95,5 @@ let stream (model : Model) (msgs: IAsyncObservable<Msg>) =
 
 printf "Starting program"
 
-let app = Reaction.StreamView initialModel view update stream
+let app () = Reaction.StreamView initialModel view update stream
 mountById "reaction-app" (ofFunction app () [])
